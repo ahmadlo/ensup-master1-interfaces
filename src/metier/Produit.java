b6b3a1f32c7fd5d35d9c30f295813ad26f4fa024
Ahmadou Lo <ahmadou19@gmail.com>
2019-11-07 14:45:20 +0100

    Création de la classe Produit

--- src/metier/Produit.java
+++ src/metier/Produit.java
@@ -1,5 +1,40 @@
 package metier;
 
 public class Produit {
+	private int idProduit;
+	private String nomProduit;
+	private Double prix;
+	private int quantite;
+	public int getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(int idProduit) {
+		this.idProduit = idProduit;
+	}
+	public String getNomProduit() {
+		return nomProduit;
+	}
+	public void setNomProduit(String nomProduit) {
+		this.nomProduit = nomProduit;
+	}
+	public Double getPrix() {
+		return prix;
+	}
+	public void setPrix(Double prix) {
+		this.prix = prix;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	@Override
+	public String toString() {
+		return "Produit [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", prix=" + prix + ", quantite="
+				+ quantite + "]";
+	}
+	
+	
 
 }
